f0004dd77da38fdc9569bec89e6ce3f1ecf062c3
Connor McCue <connor.mccue@comcast.net>
2022-10-03 17:37:47 -0700

    gamecontext.js additions

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,8 +3,20 @@ import { createContext, useState } from 'react';
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const gameState = {
+  const [board, setBoard] = useState([]);
+  const [playerX, setPlayerX] = useState();
+  const [playerO, setPlayerO] = useState();
+  const [tile, setTile] = useState();
 
+  const gameState = {
+    board,
+    setBoard,
+    tile,
+    setTile,
+    playerX,
+    setPlayerX,
+    playerO,
+    setPlayerO
   };
 
   return <GameContext.Provider value = {{ ...gameState }}>{children}</GameContext.Provider>;
