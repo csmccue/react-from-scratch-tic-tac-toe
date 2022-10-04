418d5c11e6d23f2e6325d6fe86faf2b94d14b9bb
Connor McCue <connor.mccue@comcast.net>
2022-10-03 17:51:07 -0700

    test1

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,18 @@ import { useContext } from 'react';
 import { GameContext } from '../context/GameContext';
 
 export default function Board() {
+  const { board, setBoard, tile, setTile } = useContext(GameContext);
   return (
-    <div>Board</div>
+    <div className = "board">
+      <Tile>1</Tile>
+      <Tile>2</Tile>
+      <Tile>3</Tile>
+      <Tile>4</Tile>
+      <Tile>5</Tile>
+      <Tile>6</Tile>
+      <Tile>7</Tile>
+      <Tile>8</Tile>
+      <Tile>9</Tile>
+    </div>
   );
 }
