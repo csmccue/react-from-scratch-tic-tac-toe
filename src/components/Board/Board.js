c0f329dd771ba591705b52f1a88154e7d0c838a2
Connor McCue <connor.mccue@comcast.net>
2022-10-04 16:21:33 -0700

    changed gamecontext, testing initial board settings and display in board.js

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,14 +5,17 @@ import { useContext } from 'react';
 import { GameContext } from '../../context/GameContext';
 
 export default function Board() {
+
   const { board, setBoard, playerX, setPlayerX, playerO, setPlayerO, active, setActive, currentPlayer, setCurrentPlayer } = useContext(GameContext);
+  console.log(...board[1][1]); //5
+  console.log(...board[0][0]); //1
   return (
     <main>
       <div className = "message">WELCOME TO ALCHEMY TIC TAC-O</div>
       <div className = "message">It is {currentPlayer}s turn</div>
       <div className = "board">
         <div className="row">
-          <Tile />
+          <Tile {...board[0][0]} />
           <Tile />
           <Tile />
         </div>
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,12 +3,12 @@ import { createContext, useState } from 'react';
 const GameContext = createContext();
 
 const initialBoard = 
-  ['', '', '', 
-    '', '', '', 
-    '', '', ''];
+  [['1', '2', '3'], 
+    ['4', '5', '6'], 
+    ['7', '8', '9']];
 
 const GameProvider = ({ children }) => {
-  const [board, setBoard] = useState([initialBoard]);
+  const [board, setBoard] = useState(initialBoard);
   const [playerX, setPlayerX] = useState();
   const [playerO, setPlayerO] = useState();
   const [tile, setTile] = useState();
