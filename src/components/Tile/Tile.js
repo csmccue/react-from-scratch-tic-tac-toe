f864cf2a06e9d1829500c7a5d5c671f0326cf6fb
Connor McCue <connor.mccue@comcast.net>
2022-10-04 17:47:32 -0700

    tile uses active state to enable click

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,25 +3,25 @@ import { useContext } from 'react';
 import { GameContext } from '../../context/GameContext';
 import './Tile.css';
 
-const tileType = {
-  circle: 'O',
-  ex: 'X',
-};
-
 
 export default function Tile({ location }) {
-  const { board, setBoard, playerX, setPlayerX, playerO, setPlayerO, active, setActive, currentPlayer, setCurrentPlayer } = useContext(GameContext);
+  const { board, active, currentPlayer, setCurrentPlayer } = useContext(GameContext);
   
   function handleTileClick() {
-    board[location].value = currentPlayer;
-    if (currentPlayer === 'X') {
-      setCurrentPlayer('O');
+    if (!active) {
+      return;
     } else {
-      setCurrentPlayer('X');
+      board[location].value = currentPlayer;
+      if (currentPlayer === 'X') {
+        setCurrentPlayer('O');
+      } else {
+        setCurrentPlayer('X');
+      }
+      console.log('you clicked!');
+      console.log('it is now ' + currentPlayer + ' turn');
     }
-    console.log('you clicked!');
-    console.log('it is now ' + currentPlayer + ' turn');
   }
+
 
   return (
     <div className= "tile" onClick={handleTileClick}>
