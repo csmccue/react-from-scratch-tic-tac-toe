c6ea03d1b2d481641d5dd1a8c0d498c079ff0ffc
Connor McCue <connor.mccue@comcast.net>
2022-10-13 15:37:23 -0700

    fixed some game logic

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -11,15 +11,15 @@ export default function Tile({ location }) {
     if (!active) {
       return;
     } else {
-      if (currentPlayer === 'X') {
-        setCurrentPlayer('O');
-      } else {
-        setCurrentPlayer('X');
-      }
+
       setBoard((prevState) => {
         return prevState.map((space)=>{
-          if (space.place === location) {
-            return { place: location, value: currentPlayer };
+          if (space.place - 1 === location && space.value === '') {
+            if (currentPlayer === 'X') {
+              setCurrentPlayer('O');
+            } else {
+              setCurrentPlayer('X');
+            } return { place: location, value: currentPlayer };
           } else {
             return { ...space };
           }
